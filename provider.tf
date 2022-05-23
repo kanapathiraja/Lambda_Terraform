db9c7efa4231510ebba7b9c3f8e111cde0e657f2
kanapathiraja <83782249+kanapathiraja@users.noreply.github.com>
2022-05-23 17:08:38 +0530

    updated

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = AKIARRSXKNBI5B2P22NG
-  secret_key = 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
+  access_key = "AKIARRSXKNBI5B2P22NG"
+  secret_key = "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"
 }
 
 terraform {
